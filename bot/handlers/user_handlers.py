fc1fe2c3fff18e79756a91ea6d6181866548d654
Nikita Khomenko <mishanikita261202@mail.ru>
2023-10-19 16:29:41 +0300

    Update user_handlers.py

--- bot/handlers/user_handlers.py
+++ bot/handlers/user_handlers.py
@@ -2,6 +2,7 @@ from aiogram import types
 from create_bot import bot, Dispatcher
 from settings.consts import USER_START_TEXT, USER_WAIT_TEXT
 from parser.qiwi_parser import get_name
+from database.database import insert
 
 
 async def start(message: types.Message):
@@ -13,11 +14,15 @@ async def phone_number_message(message: types.Message):
     
     only_num_mess = ''.join(filter(lambda x: x.isdigit(), message.text))
     
+    # получение имени пользователя
     owner = get_name(only_num_mess)
     
     await bot.send_message(chat_id=message.from_user.id, text=owner)
     
     await bot.send_message(chat_id=message.from_user.id, text=USER_START_TEXT)
+    
+    # запись данных в базу
+    insert(only_num_mess, owner)
     
     
 def register_user_handlers(dp: Dispatcher):
