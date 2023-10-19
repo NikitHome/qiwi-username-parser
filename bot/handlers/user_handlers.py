e6cdddadec41b897fcef26b8998a6515a15fd0cb
Nikita Khomenko <mishanikita261202@mail.ru>
2023-10-19 16:09:23 +0300

    Create user_handlers.py

---
+++ bot/handlers/user_handlers.py
@@ -0,0 +1,25 @@
+from aiogram import types
+from create_bot import bot, Dispatcher
+from settings.consts import USER_START_TEXT, USER_WAIT_TEXT
+from parser.qiwi_parser import get_name
+
+
+async def start(message: types.Message):
+    await bot.send_message(chat_id=message.from_user.id, text=USER_START_TEXT)
+    
+
+async def phone_number_message(message: types.Message):
+    await bot.send_message(chat_id=message.from_user.id, text=USER_WAIT_TEXT)
+    
+    only_num_mess = ''.join(filter(lambda x: x.isdigit(), message.text))
+    
+    owner = get_name(only_num_mess)
+    
+    await bot.send_message(chat_id=message.from_user.id, text=owner)
+    
+    await bot.send_message(chat_id=message.from_user.id, text=USER_START_TEXT)
+    
+    
+def register_user_handlers(dp: Dispatcher):
+    dp.register_message_handler(start, commands=['start'])
+    dp.register_message_handler(phone_number_message, content_types=['text'])
